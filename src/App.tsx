5bbaf6c8a509e87abe53f71e524f71e950ea3a3d
christian sanabria <ckevin89@hotmail.com>
2023-09-05 12:33:50 -0500

    jsx project started

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Navigation from "./components/Navigation";
+
+export default function App() {
+  const name:string = 'Kevin';
+  const isLoggedIn:boolean = true;
+
+  const posts: {id:number, title:string}[] = [
+      {id: 1, title: 'Happy Tuesday'},
+      {id: 2, title: 'How was your weekend?'},
+      {id: 3, title: 'I love React!'}
+  ];
+
+  return (
+      <div>
+        <Navigation isLoggedIn={isLoggedIn}/>
+          { isLoggedIn ? <h1>Hello {name}!</h1> : <h1>Hello and Welcome</h1>}
+          {posts.map(p => <li key={p.id}>{p.title}</li>)}
+      </div>
+  )
+}
