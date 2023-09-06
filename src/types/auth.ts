1955200b0ea08080e36c9c1f5662866f8afa1ad6
christian sanabria <ckevin89@hotmail.com>
2023-09-06 12:01:22 -0500

    updates

---
+++ src/types/auth.ts
@@ -0,0 +1,12 @@
+
+
+type UserType = {
+    id: number,
+    username: string,
+    password?: string,
+    first_name: string,
+    last_name: string,
+    email: string
+}
+
+export default UserType
